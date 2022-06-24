279b2184e03a5f25999846dce3ad9adcc9719fa6
DanielNunezVera <dananuez@misena.edu.co>
2022-06-23 22:30:48 -0500

    Se añade interfaz actualizar pacientes

--- view/update pacientes/update_pacientes.php
+++ view/update pacientes/update_pacientes.php
@@ -1,5 +1,29 @@
+<?php
+    include "../../controller/conexion-db/db_connect.php";
+
+    include "../../controller/sesiones/sesiones_pac.php";
+
+    $consulta = "SELECT * FROM personas INNER JOIN pacientes ON personas.id_pers = pacientes.id_pac";
+
+    $resultado = mysqli_query($conectar, $consulta) or die(mysqli_error());
+
+    $fila = mysqli_fetch_assoc($resultado);
+
+    $filas = mysqli_num_rows($resultado);
+
+    if($filas == 1){
+        
+        $_SESSION['id_pac'] = $fila['id_pac'];
+        $_SESSION['genero'] = $fila['genero'];
+        $_SESSION['fecha_nacimiento'] = $fila['fecha_nacimiento'];
+        $_SESSION['creacion'] = $fila['creacion'];
+        $_SESSION['actualizacion'] = $fila['actualizacion'];
+        $_SESSION['delete'] = $fila['delete'];
+    }
+
+?>
 <!DOCTYPE html>
-<html lang="en"> 
+<html lang="es"> 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,7 +33,7 @@
     <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="assets/css/updatepatientstyles.css">
+    <link rel="stylesheet" href="assets/css/updatepacientesstyles.css">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@800&display=swap');
     </style>
@@ -27,7 +51,7 @@
                     <img src="../../assets/images/ajustes.png" alt="">
                     <ul>
                         <li><a href="../pacientes/index_pac.php">Inicio</a></li>
-                        <li><a href="../Update patient admin/update_patient_admin.php">Actualizar datos</a></li>
+                        <li><a href="../Update pacientes/update_pacientes.php">Actualizar datos</a></li>
                         <li><a href="../../controller/sesiones/cerrarsesion.php">Cerrar sesion</a></li>
                     </ul>
                 </nav>
@@ -42,19 +66,19 @@
                     <div class="contact-wrapper animated bounceInUp">
                         <div class="contact-form">
                             <form action="" method="POST">
-                                <p>
+                                <!-- <p>
                                     <label>Tipo de documento</label>
                                     <input type="text" name="t_doc" disabled>
-                                </p>
+                                </p> -->
                                 <p>
                                     <label>N° documento</label>
-                                    <input type="number" name="num_doc" disabled>
+                                    <input type="number" name="num_doc" value="<?php echo $_SESSION['num_doc_pers'];?>" disabled>
                                 </p>
                                 <p>
-                                    <label>Primer nombre</label>
-                                    <input type="text" name="p_nombre" required>
+                                    <label>Nombre completo</label>
+                                    <input type="text" name="fullnombre" value="<?php echo $_SESSION['p_nombre_pers']."".$_SESSION['s_nombre_pers']."".$_SESSION['p_apellido_pers']."".$_SESSION['s_apellido_pers'];?>" disabled>
                                 </p>
-                                <p>
+                                <!-- <p>
                                     <label>Segundo nombre</label>
                                     <input type="text" name="s_nombre">
                                 </p>
@@ -65,22 +89,27 @@
                                 <p>
                                     <label>Segundo apellido</label>
                                     <input type="text" name="s_apellido">
+                                </p> -->
+                                <p>
+                                    <label>Email</label>
+                                    <input type="email" name="email" value="<?php echo $_SESSION['correo_pers']?>" required>
                                 </p>
                                 <p>
                                     <label>Teléfono</label>
-                                    <input type="tel" name="tel" required>
-                                </p>
-                                <p>
-                                    <label>Email</label>
-                                    <input type="email" name="email" required>
+                                    <input type="tel" name="tel" value="<?php echo $_SESSION['tel_pers'];?>" required>
                                 </p>
                                 <p>
                                     <label>Fecha de nacimiento</label>
-                                    <input type="date" name="f_nacimiento" required>
+                                    <input type="text" name="f_nacimiento" value="<?php echo date('d/m/Y',strtotime($_SESSION['fecha_nacimiento']));?>" disabled>
                                 </p>
                                 <p class="block">
-                                    <button name="registrar" type="submit">
-                                        Registrar
+                                    <button name="update" type="submit">
+                                        Actualizar
+                                    </button>
+                                </p>
+                                <p class="block">
+                                    <button name="update" type="submit">
+                                        Actualizar
                                     </button>
                                 </p>
                             </form>
